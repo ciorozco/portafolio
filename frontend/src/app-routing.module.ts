30fc282fe6317adf73c6d4dfa066fb937d9527f8
ciorozco <ciorozco.unsa@gmail.com>
2022-03-11 02:28:52 -0300

    Add files via upload

---
+++ frontend/src/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
+
+
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'sobre-mi', component: AboutComponent},
+  {path: 'proyectos', component: ProjectsComponent},
+  {path: 'crear-proyecto', component: CreateComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: 'detalle-proyecto/:id', component: DetailComponent},
+  {path: 'editar-proyecto/:id', component: EditComponent},
+  {path: '**', component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
